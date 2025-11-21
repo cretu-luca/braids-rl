956a20a79ea3924336516af348de035aef9cc957
luca <lucac375@gmail.com>
2025-11-21 14:51:59 +0200

    config class

--- src/config.py
+++ src/config.py
@@ -3,9 +3,22 @@ import os
 class Configuration:
     N_STRANDS = 3
     
-    DATA_DIR = "../data/"
-    MODEL_DIR = "../model/"
-    LOG_DIR = "../log/"
+    DATA_DIR = "data/"
+    MODEL_DIR = "/model/"
+    LOG_DIR = "/log/"
+
+    LEARNING_RATE = 0.0003
+    ENTROPY_COEF = 0.01
+    TOTAL_TIMESTEPS = 300_000
+    
+    REWARD_STEP = -0.05
+    REWARD_INVALID = -1.0
+    REWARD_LOOP = -2.0
+    REWARD_SOLVED = 20.0
+    REWARD_SHRINK = 1.0
+    REWARD_GROW = -1.0
+
+    MAX_INFERENCE_STEPS = 50
 
     @staticmethod
     def get_dataset_path(level_name):
